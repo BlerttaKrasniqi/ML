30d71d72ca051938bcb68d304fa44d6a196afe3e
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-07-22 23:47:18 +0200

    ML-2: Importing the libraries

---
+++ diabetes.py
@@ -0,0 +1,6 @@
+import numpy 
+import pandas
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.metrics import accuracy_score 

a9728d0b501cd3f2576337a108ab3255ed406b71
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-08-01 21:25:38 +0200

    ML-17: Training data and testing data

--- parkinsons.py
+++ parkinsons.py
@@ -30,3 +30,21 @@ scaler.fit(x)
 std_data = scaler.transform(x)
 #print(std_data)
 x = std_data
+
+#-------------------------------
+
+#Ndarja e te dhenave ne training data dhe test data
+
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.3,stratify=y,random_state=2)
+
+classifier = svm.SVC(kernel='linear')
+classifier.fit(x_train,y_train)
+
+x_train_prediction = classifier.predict(x_train)
+train_accuracy = accuracy_score(x_train_prediction,y_train)
+print("Train accuracy score: ",train_accuracy)
+
+x_test_prediction = classifier.predict(x_test)
+test_accuracy = accuracy_score(x_test_prediction,y_test)
+print("Test accuracy score: ",test_accuracy)
+

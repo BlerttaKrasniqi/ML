b92fdb139e7f0ced6000907acc766f5ce0c15134
BlerttaKrasniqi <122098017+BlerttaKrasniqi@users.noreply.github.com>
2024-08-07 22:18:27 +0200

    ML-21

---
+++ server/app.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, jsonify
+from diabetes import load_model, make_prediction
+from heart_disease import load_model as load_heart_model,make_prediction as heart_prediction
+from parkinsons import load_model as parkinsons_load_model, make_prediction as parkinsons_prediction
+
+app = Flask(__name__)
+
+# Load the model, scaler, and encoder once at the start
+model, scaler, encoder = load_model()
+heart_model,heart_scaler = load_heart_model()
+parkinsons_model,parkinsons_scaler = parkinsons_load_model()
+
+@app.route('/')
+def home():
+    return "Health Prediction API"
+
+@app.route('/predict_diabetes', methods=['POST'])
+def predict_diabetes():
+    if request.method == 'POST':
+        try:
+            # Get the data from the POST request
+            data = request.get_json(force=True)
+            user_input = data['input']
+            
+            # Call the predict function from diabetes.py
+            prediction = make_prediction(model, scaler, encoder, user_input)
+            
+            # Return the prediction result as JSON
+            return jsonify({
+                'prediction': int(prediction[0]),
+                'message': 'The person has diabetes' if prediction[0] == 1 else 'The person does not have diabetes'
+            })
+        except Exception as e:
+            return jsonify({'error': str(e)}), 400
+
+@app.route('/predict_heart_disease',methods=['POST'])
+def predict_heart_disease():
+    if request.method == 'POST':
+        try:
+            data = request.get_json(force=True)
+            user_input = data['input']
+            prediction = heart_prediction(heart_model,heart_scaler,user_input)
+            return jsonify({
+                'prediction': int(prediction[0]),
+                'message': 'The person has heart disease' if prediction[0]==1 else 'The person does not have heart disease'
+            })
+        except Exception as e:
+            return jsonify({'error': str(e)}),400
+
+@app.route('/predict_parkinsons',methods = ['POST'])
+def predict_parkinsons():
+    if request.method == 'POST':
+        try:
+            data = request.get_json(force=True)
+            user_input = data['input']
+            prediction = parkinsons_prediction(parkinsons_model,parkinsons_scaler,user_input)
+            return jsonify({
+                'prediction':int(prediction[0]),
+                'message':'The person has Parkinsons' if prediction[0]==1 else 'The person does not have Parkinsons'
+            
+            })
+        except Exception as e:
+            return jsonify({'error':str(e)}),400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
